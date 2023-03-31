c3fc6bea1371f0b71d5479fcdf1b7a18a56a3563
magicmenma <magicmenma@gmail.com>
2023-03-31 05:06:20 -0700

    Add icon_small.png to Recycler Item
    w

--- app/src/main/java/com/example/soundify/MusicListAdapter.java
+++ app/src/main/java/com/example/soundify/MusicListAdapter.java
@@ -1,4 +1,16 @@
 package com.example.soundify;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MusicListAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
