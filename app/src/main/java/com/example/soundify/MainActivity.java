b2c5b7d5d6f9a623299d3b671c2a787f25b67e3b
magicmenma <magicmenma@gmail.com>
2023-03-31 06:25:59 -0700

    Set Up the New Main Activity
    w

--- app/src/main/java/com/example/soundify/MainActivity.java
+++ app/src/main/java/com/example/soundify/MainActivity.java
@@ -21,10 +21,11 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
 
     ImageButton play_button, previous_button, next_button;
     SeekBar seekBar;
-    TextView currentTime, totalTime;
+    TextView titleTextView, currentTime, totalTime;
     String[] songs;
     ArrayList<AudioModel> songsList;
-    MediaPlayer mediaPlayer;
+    AudioModel currentSong;
+    MediaPlayer mediaPlayer = MyMediaPlayer.getInstance();
     private int currentTheme,textSize;
 
     boolean isPlaying = false;
@@ -49,68 +50,22 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
         play_button = findViewById(R.id.playButton);
         previous_button = findViewById(R.id.previousButton);
         next_button = findViewById(R.id.nextButton);
-        play_button.setOnClickListener(this);
-        previous_button.setOnClickListener(this);
-        next_button.setOnClickListener(this);
 
+        titleTextView = findViewById(R.id.songsname);
         seekBar = findViewById(R.id.seekBar);
         currentTime = findViewById(R.id.currentTime);
         totalTime = findViewById(R.id.totalTime);
 
+        titleTextView.setSelected(true);
+
         songsList = (ArrayList<AudioModel>) getIntent().getSerializableExtra("LIST");
+
+        setResourcesWithMusic();
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Toast.makeText(this, "onCreate was called", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onClick(View view){
-        if(view.getId() == R.id.playButton){
-            isPause = !isPause;
-            if(!isPlaying){
-                Toast.makeText(this, R.string.play_button, Toast.LENGTH_SHORT).show();
-                setupMusic();
-                isPause = false;
-                isPlaying = true;
-                mediaPlayer.start();
-
-                timer++; //track time
-            }
-            if(isPause) {
-                Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
-                mediaPlayer.pause();
-            }
-
-            if(!isPause) {
-                mediaPlayer.start();
-            }
-        }
-
-        if(view.getId() == R.id.previousButton){
-            System.out.println(trackNum);
-            Toast.makeText(this, "Previous", Toast.LENGTH_SHORT).show();
-            trackNum-=1;
-            if(trackNum == 0) trackNum = 5;
-            mediaPlayer.release();
-            setupMusic();
-            mediaPlayer.start();
-
-            timer = 0;  //track time
-        }
-        if(view.getId() == R.id.nextButton){
-            System.out.println(trackNum);
-            Toast.makeText(this, "Next", Toast.LENGTH_SHORT).show();
-            trackNum+=1;
-            if(trackNum == 5) trackNum = 1;
-            mediaPlayer.release();
-            setupMusic();
-            mediaPlayer.start();
-
-            timer = 0;  //track time
-        }
     }
 
     private void applySettings(int currentTheme, int textSize){
@@ -125,24 +80,114 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
         }
     }
 
-    public void setupMusic(){
-        try {
-            mediaPlayer = new MediaPlayer();
-            int resID = getResources().getIdentifier(songs[trackNum-1],"raw",getPackageName());
-            mediaPlayer.reset();
-            //mediaPlayer.setDataSource(songs[trackNum-1]);
-            //mediaPlayer.setDataSource(songs[i])中的songs[i] 是字符串数组中每首歌曲的名字，而非音频文件的路径
-            mediaPlayer.setDataSource(getApplicationContext(), Uri.parse("android.resource://" + getPackageName() + "/" + resID));
-            mediaPlayer.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void setResourcesWithMusic(){
+        currentSong = songsList.get(MyMediaPlayer.currentIndex);
+
+        titleTextView.setText(currentSong.getTitle());
+
+        totalTime.setText(convertToMMSS(currentSong.getDuration()));
+
+        play_button.setOnClickListener(v-> pausePlay());
+        next_button.setOnClickListener(v-> playNextSong());
+        previous_button.setOnClickListener(v-> playPreviousSong());
+
+        playMusic();
+    }
+
+    private void playMusic(){
+        mediaPlayer.reset();
+        mediaPlayer.setDataSource(currentSong.getPath());
+        mediaPlayer.prepare();
+        mediaPlayer.s
+    }
+
+    private void playNextSong(){
+
+    }
+
+    private void playPreviousSong(){
+
+    }
+
+    private void pausePlay(){
+
     }
 
     public static String convertToMMSS(String duration){
         Long millis = Long.parseLong(duration);
         return String.format("%02d:%02d",
                 TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
-                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.HOURS.toSeconds(1));
+                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1));
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+//    @Override
+//    public void onClick(View view){
+//        if(view.getId() == R.id.playButton){
+//            isPause = !isPause;
+//            if(!isPlaying){
+//                Toast.makeText(this, R.string.play_button, Toast.LENGTH_SHORT).show();
+//                setupMusic();
+//                isPause = false;
+//                isPlaying = true;
+//                mediaPlayer.start();
+//
+//                timer++; //track time
+//            }
+//            if(isPause) {
+//                Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+//                mediaPlayer.pause();
+//            }
+//
+//            if(!isPause) {
+//                mediaPlayer.start();
+//            }
+//        }
+//
+//        if(view.getId() == R.id.previousButton){
+//            System.out.println(trackNum);
+//            Toast.makeText(this, "Previous", Toast.LENGTH_SHORT).show();
+//            trackNum-=1;
+//            if(trackNum == 0) trackNum = 5;
+//            mediaPlayer.release();
+//            setupMusic();
+//            mediaPlayer.start();
+//
+//            timer = 0;  //track time
+//        }
+//        if(view.getId() == R.id.nextButton){
+//            System.out.println(trackNum);
+//            Toast.makeText(this, "Next", Toast.LENGTH_SHORT).show();
+//            trackNum+=1;
+//            if(trackNum == 5) trackNum = 1;
+//            mediaPlayer.release();
+//            setupMusic();
+//            mediaPlayer.start();
+//
+//            timer = 0;  //track time
+//        }
+//    }
+
+    //    public void setupMusic(){
+//        try {
+//            mediaPlayer = new MediaPlayer();
+//            int resID = getResources().getIdentifier(songs[trackNum-1],"raw",getPackageName());
+//            mediaPlayer.reset();
+//            //mediaPlayer.setDataSource(songs[trackNum-1]);
+//            //mediaPlayer.setDataSource(songs[i])中的songs[i] 是字符串数组中每首歌曲的名字，而非音频文件的路径
+//            mediaPlayer.setDataSource(getApplicationContext(), Uri.parse("android.resource://" + getPackageName() + "/" + resID));
+//            mediaPlayer.prepare();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
